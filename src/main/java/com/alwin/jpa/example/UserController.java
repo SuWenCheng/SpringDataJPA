90cee2d3b3ff47c97d70360eb7d9883e0330d63e
suwencheng <wencheng@imagedt.com>
2020-10-14 00:29:24 +0800

    PagingAndSortingRepository

--- src/main/java/com/alwin/jpa/example/UserController.java
+++ src/main/java/com/alwin/jpa/example/UserController.java
@@ -2,9 +2,13 @@ package com.alwin.jpa.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/user/")
@@ -22,5 +26,18 @@ public class UserController {
     @ResponseBody
     public Page<User> getAllUsers(Pageable request) {
         return userRepository.findAll(request);
+    }
+
+    @GetMapping(path = "page")
+    @ResponseBody
+    public Page<User> getAllUserByPage() {
+        return userRepository.findAll(
+                PageRequest.of(1,  20, Sort.by(new Sort.Order(Sort.Direction.ASC, "name"))));
+    }
+
+    @GetMapping(path = "sort")
+    @ResponseBody
+    public List<User> getAllUsersWithSort() {
+        return userRepository.findAll(Sort.by(new Sort.Order(Sort.Direction.ASC,"name")));
     }
 }
